d77facffb9c60e4a56e3bb8c8ec16798b5db4027
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-19 19:42:46 +0900

    feat(component/SettingsMenu): クラス名を追加

--- src/client/components/SettingsMenu.js
+++ src/client/components/SettingsMenu.js
@@ -4,17 +4,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const SettingsMenu = () => (
-  <div>
-    <div>
+  <div className="settings-menu">
+    <div className="menu-item">
       <Link to="/settings/profile">公開用プロフィール</Link>
     </div>
-    <div>
+    <div className="menu-item">
       <Link to="/settings/account">アカウント</Link>
     </div>
-    <div>
+    <div className="menu-item">
       <Link to="/settings/password">パスワード</Link>
     </div>
-    <div>
+    <div className="menu-item">
       <Link to="/settings/notifications">メールアドレスと通知</Link>
     </div>
   </div>
