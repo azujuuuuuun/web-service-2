0c8e932aaef6856cf6726e4f7f71bb450037a1d0
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-20 00:27:00 +0900

    feat(component/SettingsMenu): styled-componentsを導入

--- src/client/components/SettingsMenu.js
+++ src/client/components/SettingsMenu.js
@@ -1,22 +1,39 @@
 // @flow
 
 import React from 'react';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
+const MenuItem = styled.div`
+  padding: 0.25rem 0.5rem;
+  :not(:last-child) {
+    border-bottom: 1px solid lightgray;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  color: gray;
+  font-size: 0.9rem;
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
 const SettingsMenu = () => (
-  <div className="settings-menu">
-    <div className="menu-item">
-      <Link to="/settings/profile">公開用プロフィール</Link>
-    </div>
-    <div className="menu-item">
-      <Link to="/settings/account">アカウント</Link>
-    </div>
-    <div className="menu-item">
-      <Link to="/settings/password">パスワード</Link>
-    </div>
-    <div className="menu-item">
-      <Link to="/settings/notifications">メールアドレスと通知</Link>
-    </div>
+  <div>
+    <MenuItem>
+      <StyledLink to="/settings/profile">公開用プロフィール</StyledLink>
+    </MenuItem>
+    <MenuItem>
+      <StyledLink to="/settings/account">アカウント</StyledLink>
+    </MenuItem>
+    <MenuItem>
+      <StyledLink to="/settings/password">パスワード</StyledLink>
+    </MenuItem>
+    <MenuItem>
+      <StyledLink to="/settings/notifications">メールアドレスと通知</StyledLink>
+    </MenuItem>
   </div>
 );
 
