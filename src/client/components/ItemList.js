7bda99d2bb458bcd500749b654913dee907c7b8f
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-19 14:43:20 +0900

    feat(component/ItemList): クラス名を追加

--- src/client/components/ItemList.js
+++ src/client/components/ItemList.js
@@ -11,20 +11,34 @@ type Props = {
 const ItemList = (props: Props) => {
   const { items } = props;
   return (
-    <div>
+    <div className="item-list">
       {items.map(i => (
-        <div key={i.id}>
-          <Avatar src={i.user.avatarImgSrc}>
-            {i.user.username}
-          </Avatar>
-          <div>
-            <Link to={`/${i.user.username}/items/${i.id}`}>
-              {i.title}
-            </Link>
+        <div key={i.id} className="item">
+          <div className="avatar">
+            <Avatar src={i.user.avatarImgSrc}>
+              {i.user.username}
+            </Avatar>
           </div>
-          <div>
-            <span>{i.user.username}</span>
-            <span>{i.updatedAt}</span>
+          <div className="content">
+            <div>
+              <Link
+                className="title"
+                to={`/${i.user.username}/items/${i.id}`}
+              >
+                {i.title}
+              </Link>
+            </div>
+            <div>
+              <div className="user">
+                <span>by</span>
+                <Link to={`/${i.user.username}`}>
+                  {i.user.username}
+                </Link>
+              </div>
+              <div className="date">
+                <span>{i.updatedAt}</span>
+              </div>
+            </div>
           </div>
         </div>
       ))}
