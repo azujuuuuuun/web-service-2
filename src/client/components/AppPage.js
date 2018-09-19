274ca079827e0ea016fe620c9505bbf841be58b0
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-19 14:01:20 +0900

    refactor(component/AppPage): ItemListコンポーネントに委譲

--- src/client/components/AppPage.js
+++ src/client/components/AppPage.js
@@ -2,10 +2,9 @@
 
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import { Link } from 'react-router-dom';
-import Avatar from '@material-ui/core/Avatar';
 
 import GlobalHeader from '../containers/GlobalHeaderContainer';
+import ItemList from './ItemList';
 
 type Props = {
   items: Array<any>,
@@ -18,22 +17,7 @@ const AppPage = (props: Props) => {
       <GlobalHeader />
       <Grid container justify="center">
         <Grid item xs={7}>
-          {items.map(i => (
-            <div key={i.id}>
-              <Avatar src={i.user.avatarImgSrc}>
-                {i.user.username}
-              </Avatar>
-              <div>
-                <Link to={`/${i.user.username}/items/${i.id}`}>
-                  {i.title}
-                </Link>
-              </div>
-              <div>
-                <span>{i.user.username}</span>
-                <span>{i.updatedAt}</span>
-              </div>
-            </div>
-          ))}
+          <ItemList items={items} />
         </Grid>
       </Grid>
     </div>
