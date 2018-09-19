dca7ccfd4a97ac63b5d1e9e6421b59f5691e2a7f
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-20 00:37:41 +0900

    feat(component/AccountPage): styled-componentsを導入

--- src/client/components/AccountPage.js
+++ src/client/components/AccountPage.js
@@ -1,8 +1,8 @@
 // @flow
 
 import React from 'react';
+import styled from 'styled-components';
 import Grid from '@material-ui/core/Grid';
-import Avatar from '@material-ui/core/Avatar';
 import { Link } from 'react-router-dom';
 
 import GlobalHeader from '../containers/GlobalHeaderContainer';
@@ -13,30 +13,41 @@ type Props = {
   viewer: any,
 };
 
+const Img = styled.img`
+  display: block;
+  width: 2.5rem;
+`;
+
+const StyledLink = styled(Link)`
+  font-size: 0.9rem;
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
 const AccountPage = (props: Props) => {
   const { viewer } = props;
   return (
-    <div className="account-page">
+    <div>
       <GlobalHeader />
       <Grid container justify="center" spacing={16}>
         <Grid item xs={3}>
           <SettingsMenu />
         </Grid>
         <Grid item xs={7}>
-          <div className="account">
+          <div>
             <TitleSection
               avatarImgSrc={viewer.avatarImgSrc}
               username={viewer.username}
               title="アカウント"
             />
-            <div className="icon">
+            <div>
               <div>アイコン</div>
-              <img className="img" src={viewer.avatarImgSrc} alt="アバター" />
-              <div className="upload-link">
-                <Link to="/settings/account/custom_image">
-                  画像アップロード
-                </Link>
-              </div>
+              <Img src={viewer.avatarImgSrc} alt="アバター" />
+              <StyledLink to="/settings/account/custom_image">
+                画像アップロード
+              </StyledLink>
             </div>
           </div>
         </Grid>
