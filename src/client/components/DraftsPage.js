6147d52f2aff49de20a88b4a6d36db731a8e766c
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-21 23:09:34 +0900

    refactor(component/DraftsPage): Container Componentを統合

--- src/client/components/DraftsPage.js
+++ src/client/components/DraftsPage.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import { connect } from 'react-redux';
 
 import GlobalHeader from './GlobalHeader';
 import DraftsSidebar from './DraftsSidebar';
@@ -30,4 +31,19 @@ const DraftsPage = (props: Props) => {
   );
 };
 
-export default DraftsPage;
+const mapStateToProps = (state, ownProps) => {
+  const { items } = state.viewer;
+  const draftItems = items.filter(i => i.status === 'draft');
+  const postedItems = items.filter(i => i.status === 'posted');
+  const { itemId: itemIdStr } = ownProps.match.params;
+  const itemId = Number(itemIdStr);
+  const itemArr = (itemId && items.filter(i => i.id === itemId)) || undefined;
+  const item = (itemArr && itemArr[0]) || undefined;
+  return {
+    draftItems,
+    postedItems,
+    item,
+  };
+};
+
+export default connect(mapStateToProps)(DraftsPage);
