6c63e6b719cab4985164fec3ba47dd43ae7c1d16
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-22 00:23:36 +0900

    refactor(component/FollowButton): Container Componentを統合

--- src/client/components/FollowButton.js
+++ src/client/components/FollowButton.js
@@ -2,6 +2,9 @@
 
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import { connect } from 'react-redux';
+
+import { followRequested } from '../actions';
 
 type Props = {
   followRequest: any,
@@ -13,4 +16,13 @@ const FollowButton = (props: Props) => {
   return <Button onClick={() => followRequest(user)}>フォロー</Button>;
 };
 
-export default FollowButton;
+const mapStateToProps = () => ({});
+
+const mapDispatchToProps = (dispatch: any) => ({
+  followRequest: user => dispatch(followRequested({ user })),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(FollowButton);
