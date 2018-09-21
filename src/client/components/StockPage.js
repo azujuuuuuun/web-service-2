1dd08478a6417c2bd52677b24ae7ca9672712270
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-21 23:51:46 +0900

    refactor(component/StockPage): Container Componentを統合

--- src/client/components/StockPage.js
+++ src/client/components/StockPage.js
@@ -8,7 +8,9 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import { Link } from 'react-router-dom';
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import ChatBubbleOutline from '@material-ui/icons/ChatBubbleOutline';
+import { connect } from 'react-redux';
 
+import Loading from './Loading';
 import GlobalHeader from './GlobalHeader';
 
 type Props = {
@@ -18,60 +20,66 @@ type Props = {
 const StockPage = (props: Props) => {
   const { stocks } = props;
   return (
-    <div>
-      <GlobalHeader />
-      <Grid container justify="center" spacing={16}>
-        <Grid item xs={3}>
-          <h2>
-            <FolderOpenIcon />
-            <span>ストック一覧</span>
-          </h2>
-        </Grid>
-        <Grid item xs={7}>
-          {stocks.map(
-            i =>
-              i.user && i.likers && i.comments ? (
-                <div key={i.id}>
-                  <div>
-                    <Avatar src={i.user.avatarImgSrc} alt="アバター">
-                      {i.user.username}
-                    </Avatar>
-                  </div>
-                  <div>
+    <Loading>
+      <div>
+        <GlobalHeader />
+        <Grid container justify="center" spacing={16}>
+          <Grid item xs={3}>
+            <h2>
+              <FolderOpenIcon />
+              <span>ストック一覧</span>
+            </h2>
+          </Grid>
+          <Grid item xs={7}>
+            {stocks.map(
+              i =>
+                i.user && i.likers && i.comments ? (
+                  <div key={i.id}>
                     <div>
-                      <span>
-                        <Link to={`/${i.user.username}`}>
-                          {i.user.username}
+                      <Avatar src={i.user.avatarImgSrc} alt="アバター">
+                        {i.user.username}
+                      </Avatar>
+                    </div>
+                    <div>
+                      <div>
+                        <span>
+                          <Link to={`/${i.user.username}`}>
+                            {i.user.username}
+                          </Link>
+                          が{i.createdAt}
+                          に投稿
+                        </span>
+                      </div>
+                      <div>
+                        <Link to={`/${i.user.username}/items/${i.id}`}>
+                          {i.title}
                         </Link>
-                        が{i.createdAt}
-                        に投稿
-                      </span>
+                      </div>
                     </div>
                     <div>
-                      <Link to={`/${i.user.username}/items/${i.id}`}>
-                        {i.title}
-                      </Link>
+                      <div>
+                        <ThumbUpIcon />
+                        <span>{i.likers.length}</span>
+                      </div>
+                      <div>
+                        <ChatBubbleOutline />
+                        <span>{i.comments.length}</span>
+                      </div>
                     </div>
                   </div>
-                  <div>
-                    <div>
-                      <ThumbUpIcon />
-                      <span>{i.likers.length}</span>
-                    </div>
-                    <div>
-                      <ChatBubbleOutline />
-                      <span>{i.comments.length}</span>
-                    </div>
-                  </div>
-                </div>
-              ) : (
-                <CircularProgress />
-              ),
-          )}
+                ) : (
+                  <CircularProgress />
+                ),
+            )}
+          </Grid>
         </Grid>
-      </Grid>
-    </div>
+      </div>
+    </Loading>
   );
 };
 
-export default StockPage;
+const mapStateToProps = state => ({
+  stocks: state.viewer.stocks,
+});
+
+export default connect(mapStateToProps)(StockPage);
