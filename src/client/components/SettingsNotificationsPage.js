279fc8f6373c2be5a06646a5ec8fe693d0e44ae9
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-21 23:26:16 +0900

    refactor(component/SettingsNotificationsPage): Container Componentを統合

--- src/client/components/SettingsNotificationsPage.js
+++ src/client/components/SettingsNotificationsPage.js
@@ -3,11 +3,13 @@
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import GlobalHeader from './GlobalHeader';
 import SettingsMenu from './SettingsMenu';
 import EmailSetting from '../containers/EmailSettingContainer';
 import NotificationsSetting from '../containers/NotificationsSettingContainer';
+import Loading from './Loading';
 
 type Props = {
   viewer: any,
@@ -16,27 +18,33 @@ type Props = {
 const SettingsNotificationsPage = (props: Props) => {
   const { viewer } = props;
   return (
-    <div>
-      <GlobalHeader />
-      <SettingsMenu />
+    <Loading>
       <div>
+        <GlobalHeader />
+        <SettingsMenu />
         <div>
           <div>
-            <Avatar src={viewer.avatarImgSrc}>{viewer.username}</Avatar>
-            <Link to={`/${viewer.username}`}>
-              {viewer.username}
-              アカウント
-            </Link>
+            <div>
+              <Avatar src={viewer.avatarImgSrc}>{viewer.username}</Avatar>
+              <Link to={`/${viewer.username}`}>
+                {viewer.username}
+                アカウント
+              </Link>
+            </div>
+            <div>
+              <span>メールアドレスと通知</span>
+            </div>
+            <EmailSetting />
+            <NotificationsSetting viewer={viewer} />
           </div>
-          <div>
-            <span>メールアドレスと通知</span>
-          </div>
-          <EmailSetting />
-          <NotificationsSetting viewer={viewer} />
         </div>
       </div>
-    </div>
+    </Loading>
   );
 };
 
-export default SettingsNotificationsPage;
+const mapStateToProps = state => ({
+  viewer: state.viewer,
+});
+
+export default connect(mapStateToProps)(SettingsNotificationsPage);
