c5a19734b641f22aa2115f90290a6f91b52bacb2
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-21 22:01:03 +0900

    feat(component/DraftsPreviewHeader): コンポーネントを追加

---
+++ src/client/components/DraftsPreviewHeader.js
@@ -0,0 +1,42 @@
+// @flow
+
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import CreateIcon from '@material-ui/icons/Create';
+import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+type Props = {
+  itemId: string,
+  handleClickDelete: any,
+};
+
+const StyledLink = styled(Link)`
+  border: 1px solid lightgray;
+  border-radius: 5px;
+  color: black;
+  text-decoration: none;
+  padding: 0.3rem 0.5rem;
+  &:hover {
+    background-color: lightgray;
+  }
+`;
+
+const DraftsPreviewHeader = (props: Props) => {
+  const { itemId, handleClickDelete } = props;
+  return (
+    <div>
+      <StyledLink to={`/drafts/${itemId}/edit`}>
+        <CreateIcon />
+        編集
+      </StyledLink>
+      <Button onClick={handleClickDelete}>
+        <DeleteIcon />
+        削除
+      </Button>
+    </div>
+  );
+};
+
+export default DraftsPreviewHeader;
