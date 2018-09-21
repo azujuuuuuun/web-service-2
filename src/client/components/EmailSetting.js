5d745609fd0350b75f6ed95771794984775709a3
azujuuuuuun <azujuuuuuun@gmail.com>
2018-09-22 00:21:05 +0900

    refactor(component/EmailSetting): Container Componentを統合

--- src/client/components/EmailSetting.js
+++ src/client/components/EmailSetting.js
@@ -4,14 +4,21 @@ import React from 'react';
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { Fields, reduxForm } from 'redux-form';
 import type { FieldProps, FormProps } from 'redux-form';
 
-type Props = {
+import { updateUserRequested } from '../actions';
+
+type PProps = {
   email: FieldProps,
   handleSubmit: FormProps,
 };
 
-const EmailSetting = (props: Props) => {
+type CProps = {
+  handleSubmit: any,
+};
+
+const EmailSetting = (props: PProps) => {
   const { email, handleSubmit } = props;
   return (
     <div>
@@ -26,4 +33,25 @@ const EmailSetting = (props: Props) => {
   );
 };
 
-export default EmailSetting;
+class EmailSettingContainer extends React.Component<CProps> { // eslint-disable-line
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <Fields
+        names={['email']}
+        component={EmailSetting}
+        handleSubmit={handleSubmit}
+      />
+    );
+  }
+}
+
+const onSubmit = (values, dispatch) => {
+  const { email } = values;
+  dispatch(updateUserRequested({ email }));
+};
+
+export default reduxForm({
+  form: 'email',
+  onSubmit,
+})(EmailSettingContainer);
