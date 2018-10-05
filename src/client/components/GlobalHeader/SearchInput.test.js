7b36975db0d72a5473d52baedd3535c4565c7f29
azujuuuuuun <azujuuuuuun@gmail.com>
2018-10-05 19:23:52 +0900

    test(component/GlobalHeader/SearchInput): SearchInputンポーネントのテストを追加

---
+++ src/client/components/GlobalHeader/SearchInput.test.js
@@ -0,0 +1,146 @@
+// @flow
+
+import React from 'react';
+import { mount } from 'enzyme';
+
+import { SearchInput } from './SearchInput';
+
+describe('SearchInput', () => {
+  test('renders a TextField component', () => {
+    const mockFn = jest.fn();
+    const props = {
+      searchQuery: {
+        input: {
+          value: '',
+          onChange: mockFn,
+        },
+      },
+      handleSubmit: mockFn,
+    };
+    // $FlowFixMe
+    const wrapper = mount(<SearchInput {...props} />);
+
+    expect(wrapper.find('TextField').length).toBe(1);
+  });
+
+  test('inputタグのvalue属性がpropと同じ', () => {
+    const mockFn = jest.fn();
+    const props = {
+      searchQuery: {
+        input: {
+          value: 'hoge',
+          onChange: mockFn,
+        },
+      },
+      handleSubmit: mockFn,
+    };
+    // $FlowFixMe
+    const wrapper = mount(<SearchInput {...props} />);
+
+    expect(wrapper.find('input').prop('value')).toBe('hoge');
+  });
+
+  test('inputタグのtype属性がsearch', () => {
+    const mockFn = jest.fn();
+    const props = {
+      searchQuery: {
+        input: {
+          value: 'hoge',
+          onChange: mockFn,
+        },
+      },
+      handleSubmit: mockFn,
+    };
+    // $FlowFixMe
+    const wrapper = mount(<SearchInput {...props} />);
+
+    expect(wrapper.find('input').prop('type')).toBe('search');
+  });
+
+  test('inputタグのplaceholder属性が"キーワードを入力"', () => {
+    const mockFn = jest.fn();
+    const props = {
+      searchQuery: {
+        input: {
+          value: '',
+          onChange: mockFn,
+        },
+      },
+      handleSubmit: mockFn,
+    };
+    // $FlowFixMe
+    const wrapper = mount(<SearchInput {...props} />);
+
+    expect(wrapper.find('input').prop('placeholder')).toBe('キーワードを入力');
+  });
+
+  test('simulates change event', () => {
+    const mockFn = jest.fn();
+    const props = {
+      searchQuery: {
+        input: {
+          value: '',
+          onChange: mockFn,
+        },
+      },
+      handleSubmit: mockFn,
+    };
+    // $FlowFixMe
+    const wrapper = mount(<SearchInput {...props} />);
+
+    wrapper.find('input').simulate('change');
+    expect(mockFn).toHaveBeenCalled();
+  });
+
+  test('renders a Button component', () => {
+    const mockFn = jest.fn();
+    const props = {
+      searchQuery: {
+        input: {
+          value: '',
+          onChange: mockFn,
+        },
+      },
+      handleSubmit: mockFn,
+    };
+    // $FlowFixMe
+    const wrapper = mount(<SearchInput {...props} />);
+
+    expect(wrapper.find('Button').length).toBe(1);
+  });
+
+  test('buttonタグのテキストが"検索"', () => {
+    const mockFn = jest.fn();
+    const props = {
+      searchQuery: {
+        input: {
+          value: '',
+          onChange: mockFn,
+        },
+      },
+      handleSubmit: mockFn,
+    };
+    // $FlowFixMe
+    const wrapper = mount(<SearchInput {...props} />);
+
+    expect(wrapper.find('button').text()).toBe('検索');
+  });
+
+  test('simulates click event', () => {
+    const mockFn = jest.fn();
+    const props = {
+      searchQuery: {
+        input: {
+          value: '',
+          onChange: mockFn,
+        },
+      },
+      handleSubmit: mockFn,
+    };
+    // $FlowFixMe
+    const wrapper = mount(<SearchInput {...props} />);
+
+    wrapper.find('button').simulate('click');
+    expect(mockFn).toHaveBeenCalled();
+  });
+});
